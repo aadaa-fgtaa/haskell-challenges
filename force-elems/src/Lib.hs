9b9b143c31aa02d43b06506af36c4f9382712cbc
aadaa_fgtaa <aadaa_fgtaa@pm.me>
2021-01-17 21:48:45 +0300

    Solve force-elems and inspect-forced challenges

--- force-elems/src/Lib.hs
+++ force-elems/src/Lib.hs
@@ -1,6 +1,15 @@
-module Lib
-    ( forceElems
-    ) where
+{-# LANGUAGE BlockArguments, RankNTypes #-}
+
+module Lib ( forceElems ) where
+
+newtype UCont a = UCont { runUCont :: forall r . (a -> r) -> r }
+
+instance Functor UCont where
+  fmap f x = UCont ($ runUCont x f)
+
+instance Applicative UCont where
+  pure x = UCont ($ x)
+  (<*>) = fmap . flip runUCont id
 
 forceElems :: Traversable t => t a -> t a
-forceElems = undefined
+forceElems = flip runUCont id . traverse \a -> UCont ($! a)
--- h2-inspect-forced/src/Lib.hs
+++ h2-inspect-forced/src/Lib.hs
@@ -1,7 +1,12 @@
-module Lib
-    ( Tree (..)
-    , materializeForcedBy
-    ) where
+{-# LANGUAGE BlockArguments, LambdaCase #-}
+
+module Lib ( Tree (..), materializeForcedBy ) where
+
+import System.IO.Unsafe ( unsafePerformIO )
+import Data.IORef ( newIORef, readIORef, writeIORef )
+import Data.Functor ( ($>), void )
+import Control.Exception ( evaluate )
+import Data.Bool ( bool )
 
 data Tree
     = Leaf
@@ -9,4 +14,30 @@ data Tree
     deriving (Show, Eq)
 
 materializeForcedBy :: (Tree -> Int) -> Tree -> Tree
-materializeForcedBy = undefined
+materializeForcedBy f tree_ = unsafePerformIO do
+  ctag <- newIORef False
+  vtag <- newIORef False
+
+  let
+    tagged tag v = unsafePerformIO $ writeIORef tag True $> v
+
+    tagTree = \case
+      Leaf       -> tagged ctag Leaf
+      Fork l v r -> tagged ctag $ Fork (tagTree l) (tagged vtag v) (tagTree r)
+
+    tree = tagTree tree_
+
+    isUnevaluated tag x = do
+      writeIORef tag False
+      void $ evaluate x
+      readIORef tag
+
+    trimTree n = isUnevaluated ctag n >>= \cu -> if cu
+      then pure Leaf
+      else case n of
+        Leaf       -> pure Leaf
+        Fork l v r -> Fork <$> trimTree l <*> (bool v 0 <$> isUnevaluated vtag v) <*> trimTree r
+
+  void $ evaluate $ f tree
+
+  trimTree tree
