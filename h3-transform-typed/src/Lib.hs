5ee8d75be0b55e3cd456f2671d7dc05953e09827
aadaa_fgtaa <aadaa_fgtaa@pm.me>
2021-01-25 23:17:51 +0300

    Solve transform-typed challenge

--- h3-transform-typed/src/Lib.hs
+++ h3-transform-typed/src/Lib.hs
@@ -1,19 +1,35 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs, ViewPatterns #-}
 
 module Lib where
 
-import           Data.Proxy
-import           Data.Typeable
+import Data.Proxy ( Proxy(..) )
+import Type.Reflection ( Typeable )
 
 data Scheme a where
-    Res :: Typeable a => Proxy a -> Scheme a
-    Arg :: Typeable a => Proxy a -> Scheme b -> Scheme (a -> b)
+  Res :: Typeable a => Proxy a -> Scheme a
+  Arg :: Typeable a => Proxy a -> Scheme b -> Scheme (a -> b)
 
-data Function = forall a. Function (Scheme a) a
+data Function = forall a . Function (Scheme a) a
 
-newtype Wrap a = Wrap
-    { unWrap :: a
-    }
+newtype Wrap a = Wrap { unWrap :: a }
+
+data WScheme a b where
+  WRes :: Typeable a => Proxy a -> WScheme (Wrap a) a
+  WArg :: Typeable a => Proxy a -> WScheme wb b -> WScheme (Wrap a -> wb) (a -> b)
+
+data SomeWScheme a = forall b . SomeWScheme (WScheme b a)
+
+wrapWithWScheme :: WScheme a b -> b -> a
+wrapWithWScheme (WRes _)   f = Wrap f
+wrapWithWScheme (WArg _ r) f = wrapWithWScheme r . f . unWrap
+
+toWScheme :: Scheme a -> SomeWScheme a
+toWScheme (Res p) = SomeWScheme $ WRes p
+toWScheme (Arg p (toWScheme -> SomeWScheme r)) = SomeWScheme $ WArg p r
+
+fromWScheme :: WScheme a b -> Scheme a
+fromWScheme (WRes _) = Res Proxy
+fromWScheme (WArg _ (fromWScheme -> r)) = Arg Proxy r
 
 wrapFunction :: Function -> Function
-wrapFunction = undefined
+wrapFunction (Function (toWScheme -> SomeWScheme w) f) = Function (fromWScheme w) (wrapWithWScheme w f)
