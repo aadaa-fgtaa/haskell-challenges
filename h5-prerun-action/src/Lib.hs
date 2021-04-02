ec28691f71bc3043220a692ec851b35c611b39bd
aadaa_fgtaa <aadaa_fgtaa@pm.me>
2021-04-02 11:29:10 +0300

    Solve prerun-action challenge

--- h5-prerun-action/src/Lib.hs
+++ h5-prerun-action/src/Lib.hs
@@ -1,6 +1,17 @@
-module Lib
-    ( prerun
-    ) where
+{-# LANGUAGE BlockArguments #-}
+
+module Lib ( prerun ) where
+
+import Control.Concurrent
+import Control.Exception
+import Control.Monad
 
 prerun :: (IO a -> IO (IO b)) -> IO (IO a -> IO b)
-prerun = undefined
+prerun fn = do
+  argv <- newEmptyMVar
+  lock <- newMVar ()
+  fn'  <- fn $ join $ readMVar argv
+  pure \arg -> bracket (takeMVar lock) (putMVar lock) \() -> do
+    tryTakeMVar argv
+    putMVar argv arg
+    fn'
